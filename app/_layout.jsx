000a91735ce61352caa29ad39b40d931abf54c1b
Akshay <mail.akshayks@gmail.com>
2024-04-18 00:01:34 +0530

    configured nativewind for expo

---
+++ app/_layout.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Slot } from "expo-router";
+
+const RootLayout = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">muve</Text>
+    </View>
+  );
+};
+
+export default RootLayout;
