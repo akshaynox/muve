8105f6b516653af27ad8273f24ba7c6c5888a36b
Akshay <mail.akshayks@gmail.com>
2024-04-19 00:08:45 +0530

    added basic screens

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,15 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>muve</Text>
+      <Text className="text-3xl font-pblack">muve</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" className="font-blue">
+        Go to profile
+      </Link>
     </View>
   );
 }
